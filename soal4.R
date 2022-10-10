eb5df364a871d4a136bb870f0683042d4df7369d
athraz <96050618+athraz@users.noreply.github.com>
2022-10-10 19:23:26 +0700

    Create soal4.R

---
+++ soal4.R
@@ -0,0 +1,14 @@
+x = 2
+v = 10
+
+# 4a
+dchisq(x, v)
+
+# 4b
+hist(rchisq(n = 100, v), xlab = "x", ylab = "v", main = "Histogram distribusi Chi Square dengan 100 data random")
+
+# 4c
+rataan = v
+rataan
+varian = 2 * v
+varian
