b8d057a95322612f566a9925f582ce8eaa57b376
athraz <96050618+athraz@users.noreply.github.com>
2022-10-10 19:23:07 +0700

    Create soal3.R

---
+++ soal3.R
@@ -0,0 +1,13 @@
+lambda = 0.45
+x = 6
+
+# 3a
+dpois(x, lambda)
+
+# 3b
+hist(rpois(n = 356, lambda), xlab = "x", ylab = "Frekuensi", main = "Histogram binomial Poisson selama setahun (n = 365)")
+
+# 3d
+rataan = varian = lambda
+rataan
+varian
